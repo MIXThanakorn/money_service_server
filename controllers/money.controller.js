06be9488fa7cda7013fc710300d234bebef7a6d4
MIXThanakorn <s6652410021@sau.ac.th>
2025-04-28 14:49:09 +0700

    fix money api

--- controllers/money.controller.js
+++ controllers/money.controller.js
@@ -25,3 +25,30 @@ exports.postMoney = async (req, res) => {
     });
   }
 };
+
+exports.getMoneyByType = async (req, res) => {
+  try {
+    const { userID } = req.params;
+    const result = await prisma.money_tb.findMany({
+      where: {
+        userID: parseInt(userID),
+      },
+    });
+    if (result) {
+      res.status(200).json({
+        message: "Money found successfully",
+        info: result,
+      });
+    } else {
+      res.status(404).json({
+        message: "Money not successfully",
+        info: result,
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      error: `Server Error: ${err}`,
+    });
+    console.log(err);
+  }
+};
--- routes/money.route.js
+++ routes/money.route.js
@@ -3,6 +3,7 @@ const moneycontroller = require("../controllers/money.controller");
 
 const route = express.Router();
 
+route.get("/:userID", moneycontroller.getMoneyByType);
 route.post("/", moneycontroller.postMoney);
 
 module.exports = route;
